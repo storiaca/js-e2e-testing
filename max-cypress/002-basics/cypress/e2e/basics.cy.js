a614d694f45620c0cd7bd61f7bb51083f951b75f
Aleksandar Ristic <storiaca@gmail.com>
2025-04-08 10:11:50 +0200

    Selecting By Text

--- max-cypress/002-basics/cypress/e2e/basics.cy.js
+++ max-cypress/002-basics/cypress/e2e/basics.cy.js
@@ -5,4 +5,10 @@ describe("tasks page", () => {
     cy.visit("http://localhost:5173/");
     cy.get(".main-header img");
   });
+
+  it("should display the page title", () => {
+    cy.visit("http://localhost:5173/");
+    cy.get("h1").contains("My Cypress Course Tasks");
+    //cy.contains("My Cypress Course Tasks");
+  });
 });
