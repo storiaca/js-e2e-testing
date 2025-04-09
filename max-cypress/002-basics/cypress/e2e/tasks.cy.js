8de8b7126d7b6c2eb09f0f1f00040d6f2fcec0da
Aleksandar Ristic <storiaca@gmail.com>
2025-04-09 10:21:54 +0200

    Selecting Dropdown Values

--- max-cypress/002-basics/cypress/e2e/tasks.cy.js
+++ max-cypress/002-basics/cypress/e2e/tasks.cy.js
@@ -31,4 +31,20 @@ describe("tasks management", () => {
     cy.get(".modal").contains("Add Task").click();
     cy.contains("Please provide values");
   });
+
+  it("should filter tasks", () => {
+    cy.visit("http://localhost:5173/");
+    cy.contains("Add Task").click();
+    cy.get("#title").type("New Task");
+    cy.get("#summary").type("Some description");
+    cy.get("#category").select("urgent");
+    cy.get(".modal").contains("Add Task").click();
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("moderate");
+    cy.get(".task").should("have.length", 0);
+    cy.get("#filter").select("urgent");
+    cy.get(".task").should("have.length", 1);
+    cy.get("#filter").select("all");
+    cy.get(".task").should("have.length", 1);
+  });
 });
