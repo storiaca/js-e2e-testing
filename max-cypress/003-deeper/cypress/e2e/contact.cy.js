bd24a42742a2c955c85608f9eb457367738f00fc
Aleksandar Ristic <storiaca@gmail.com>
2025-04-09 14:28:19 +0200

    Getting more direct element access via then()

--- max-cypress/003-deeper/cypress/e2e/contact.cy.js
+++ max-cypress/003-deeper/cypress/e2e/contact.cy.js
@@ -6,9 +6,13 @@ describe("contact form", () => {
     cy.get('[data-cy="contact-input-message"]').type("Hello world!");
     cy.get('[data-cy="contact-input-name"]').type("John Doe");
     cy.get('[data-cy="contact-input-email"]').type("test@example.com");
-    cy.get('[data-cy="contact-btn-submit"]')
-      .contains("Send Message")
-      .and("not.have.attr", "disabled");
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el.attr("disabled")).to.be.undefined;
+      expect(el.text()).to.eq("Send Message");
+    });
+    // cy.get('[data-cy="contact-btn-submit"]')
+    //   .contains("Send Message")
+    //   .and("not.have.attr", "disabled");
 
     cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
     cy.get("@submitBtn").click();
