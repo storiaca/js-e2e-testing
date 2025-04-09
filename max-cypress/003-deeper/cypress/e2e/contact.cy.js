20c96e271c2647bba541078ac8807aba968cea18
Aleksandar Ristic <storiaca@gmail.com>
2025-04-09 13:30:15 +0200

    Chaininig Commands

--- max-cypress/003-deeper/cypress/e2e/contact.cy.js
+++ max-cypress/003-deeper/cypress/e2e/contact.cy.js
@@ -6,13 +6,12 @@ describe("contact form", () => {
     cy.get('[data-cy="contact-input-message"]').type("Hello world!");
     cy.get('[data-cy="contact-input-name"]').type("John Doe");
     cy.get('[data-cy="contact-input-email"]').type("test@example.com");
-    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
-    cy.get('[data-cy="contact-btn-submit"]').should(
-      "not.have.attr",
-      "disabled"
-    );
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains("Send Message")
+      .and("not.have.attr", "disabled");
     cy.get('[data-cy="contact-btn-submit"]').click();
-    cy.get('[data-cy="contact-btn-submit"]').contains("Sending...");
-    cy.get('[data-cy="contact-btn-submit"]').should("have.attr", "disabled");
+    cy.get('[data-cy="contact-btn-submit"]')
+      .contains("Sending...")
+      .should("have.attr", "disabled");
   });
 });
