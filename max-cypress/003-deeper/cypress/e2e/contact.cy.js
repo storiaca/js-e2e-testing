d69cc8937a1eddb9e2e0e6885fb2bbd167fce788
Aleksandar Ristic <storiaca@gmail.com>
2025-04-10 11:58:42 +0200

    Another example for form testing

--- max-cypress/003-deeper/cypress/e2e/contact.cy.js
+++ max-cypress/003-deeper/cypress/e2e/contact.cy.js
@@ -19,4 +19,14 @@ describe("contact form", () => {
     cy.get("@submitBtn").contains("Sending...");
     cy.get("@submitBtn").should("have.attr", "disabled");
   });
+
+  it("should valiate the form input", () => {
+    cy.visit("http://localhost:5173/about");
+    cy.get('[data-cy="contact-btn-submit"]').click();
+    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+      expect(el).to.not.have.attr("disabled");
+      expect(el.text()).to.not.equal("Sending...");
+    });
+    cy.get('[data-cy="contact-btn-submit"]').contains("Send Message");
+  });
 });
