0df3091578406b9b2a5b6465c1de379179960282
Aleksandar Ristic <storiaca@gmail.com>
2025-04-16 19:37:27 +0200

    Creating a custom command

--- max-cypress/004-conf-custom/cypress/e2e/contact.cy.js
+++ max-cypress/004-conf-custom/cypress/e2e/contact.cy.js
@@ -16,20 +16,20 @@ describe("contact form", () => {
     // Runs after all tests (i.e. only once)
   });
   it("should submit the form", () => {
-    cy.get('[data-cy="contact-input-message"]').type("Hello world!");
-    cy.get('[data-cy="contact-input-name"]').type("John Doe");
-    cy.get('[data-cy="contact-btn-submit"]').then((el) => {
+    cy.getById("contact-input-message").type("Hello world!");
+    cy.getById("contact-input-name").type("John Doe");
+    cy.getById("contact-btn-submit").then((el) => {
       expect(el.attr("disabled")).to.be.undefined;
       expect(el.text()).to.eq("Send Message");
     });
     cy.screenshot();
-    cy.get('[data-cy="contact-input-email"]').type("test@example.com{enter}");
+    cy.getById("contact-input-email").type("test@example.com{enter}");
     cy.submitForm();
     // cy.get('[data-cy="contact-btn-submit"]')
     //   .contains('Send Message')
     //   .should('not.have.attr', 'disabled');
     cy.screenshot();
-    cy.get('[data-cy="contact-btn-submit"]').as("submitBtn");
+    cy.getById("contact-btn-submit").as("submitBtn");
     // cy.get('@submitBtn').click();
     cy.get("@submitBtn").contains("Sending...");
     cy.get("@submitBtn").should("have.attr", "disabled");
--- max-cypress/004-conf-custom/cypress/support/commands.js
+++ max-cypress/004-conf-custom/cypress/support/commands.js
@@ -26,3 +26,11 @@
 Cypress.Commands.add("submitForm", () => {
   cy.get('form button[type="submit"]').click();
 });
+
+Cypress.Commands.addQuery("getById", (id) => {
+  const getFn = cy.now("get", `[data-cy="${id}"]`);
+  return () => {
+    // const element = getFn();
+    return getFn();
+  };
+});
