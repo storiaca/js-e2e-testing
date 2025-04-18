bf4dc3fdd12c659dd964532c7241ef4c002e1df5
Aleksandar Ristic <storiaca@gmail.com>
2025-04-18 14:17:54 +0200

    Stubs with Return Values

--- max-cypress/005-stubs-spies/cypress/e2e/location.cy.js
+++ max-cypress/005-stubs-spies/cypress/e2e/location.cy.js
@@ -15,6 +15,9 @@ describe("share location", () => {
             });
           }, 100);
         });
+      cy.stub(win.navigator.clipboard, "writeText")
+        .as("saveToClipboard")
+        .resolves();
     });
   });
   it("should fetch the user location", () => {
@@ -28,5 +31,7 @@ describe("share location", () => {
   it("should share a location URL", () => {
     cy.get('[data-cy="name-input"]').type("John Doe");
     cy.get('[data-cy="get-loc-btn"]').click();
+    cy.get('[data-cy="share-loc-btn"]').click();
+    cy.get("@saveToClipboard").should("have.been.called");
   });
 });
