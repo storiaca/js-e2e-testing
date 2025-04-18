05ffc3ce63a53e5a98d7ade7d45b6fa1eb140f4d
Aleksandar Ristic <storiaca@gmail.com>
2025-04-18 13:42:35 +0200

    Adding a Fake Stub Implementation

--- max-cypress/005-stubs-spies/cypress/e2e/location.cy.js
+++ max-cypress/005-stubs-spies/cypress/e2e/location.cy.js
@@ -3,14 +3,23 @@
 describe("share location", () => {
   it("should fetch the user location", () => {
     cy.visit("/").then((win) => {
-      cy.stub(win.navigator.geolocation, "getCurrentPosition").as(
-        "getUserPosition"
-      );
+      cy.stub(win.navigator.geolocation, "getCurrentPosition")
+        .as("getUserPosition")
+        .callsFake((cb) => {
+          setTimeout(() => {
+            cb({
+              coords: {
+                latitude: 37.5,
+                longitude: 48.01,
+              },
+            });
+          }, 100);
+        });
     });
     cy.get('[data-cy="get-loc-btn"]').click();
     cy.get("@getUserPosition").should("have.been.called");
     // button is disabled
     cy.get('[data-cy="get-loc-btn"]').should("be.disabled");
-    cy.get('[data-cy="actions"]').should("contain", "Location fertched"); // alternative to contains()
+    cy.get('[data-cy="actions"]').should("contain", "Location fetched"); // alternative to contains()
   });
 });
