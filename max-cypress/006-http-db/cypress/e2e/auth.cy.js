efa03c891047df158584f9966a7ad1f8398573c4
Aleksandar Ristic <storiaca@gmail.com>
2025-04-25 14:17:30 +0200

    Adding a Login test

--- max-cypress/006-http-db/cypress/e2e/auth.cy.js
+++ max-cypress/006-http-db/cypress/e2e/auth.cy.js
@@ -13,4 +13,14 @@ describe("Auth", () => {
     cy.location("pathname").should("eq", "/takeaways");
     cy.getCookie("__session").its("value").should("not.be.empty");
   });
+
+  it("should login", () => {
+    cy.visit("/login");
+    cy.get('[data-cy="auth-email').click();
+    cy.get('[data-cy="auth-email').type("test@example.com");
+    cy.get('[data-cy="auth-password').type("testpassword");
+    cy.get('[data-cy="auth-submit').click();
+    cy.location("pathname").should("eq", "/takeaways");
+    cy.getCookie("__session").its("value").should("not.be.empty");
+  });
 });
